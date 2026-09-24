fbc2939e352f534a6bcf17983ef7435c05a40e20
Preethi <preethi.radh@gmail.com>
2026-09-24 12:17:34 +0530

    Add multi-train Redis cache

--- trains/live_train_cache.py
+++ trains/live_train_cache.py
@@ -24,3 +24,20 @@ def get_live_train(train_number):
 
 def refresh_live_train_cache(data):
     save_live_train(data.train_number, data)
+
+def refresh_all_live_trains_cache(data_list):
+    for data in data_list:
+        refresh_live_train_cache(data)
+
+def get_all_live_trains():
+    keys = redis_client.keys("train:*:live")
+
+    live_trains = []
+
+    for key in keys:
+        cached_data = redis_client.get(key)
+
+        if cached_data:
+            live_trains.append(json.loads(cached_data))
+
+    return live_trains
