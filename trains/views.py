6687658b5fad0a7f3f0fdce2c5bf24c1c95da561
Preethi <preethi.radh@gmail.com>
2026-09-25 12:19:36 +0530

    Add Redis caching to all live trains API

--- trains/views.py
+++ trains/views.py
@@ -7,7 +7,10 @@ from .serializers import TrainPositionSerializer
 from .serializers import LiveTrainDataSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from .live_train_cache import get_live_train, refresh_live_train_cache
+from .live_train_cache import (get_live_train, 
+                               refresh_live_train_cache,
+                               get_all_live_trains,
+                               refresh_all_live_trains_cache)
 
 logger = logging.getLogger(__name__)
 
@@ -48,19 +51,32 @@ def train_live_position(request, train_number):
 @api_view(['GET'])
 def live_trains(request):
     try:
+        cached_data = get_all_live_trains()
+
+        if cached_data:
+            return Response(cached_data)
+
         provider = get_live_train_provider()
 
         data = provider.get_live_trains()
+
+        if not data:
+            return Response(
+                {'message': 'No active trains found.'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        refresh_all_live_trains_cache(data)
 
         serializer = LiveTrainDataSerializer(data, many=True)
 
         return Response(serializer.data)
 
     except Exception as e:
-     logger.error("Error retrieving live train data: %s", e)
-     return Response(
-        {'message': 'Unable to retrieve live train data.'},
-        status=status.HTTP_503_SERVICE_UNAVAILABLE
-     )
+        logger.error("Error retrieving live train data: %s", e)
+        return Response(
+            {'message': 'Unable to retrieve live train data.'},
+            status=status.HTTP_503_SERVICE_UNAVAILABLE
+        )
 
 # Create your views here.
